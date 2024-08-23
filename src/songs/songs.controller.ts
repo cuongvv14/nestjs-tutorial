0ffc84848ec500724f34689d3726e29683308bd9
LowkyVu <cuongmc1404@gmail.com>
2024-08-23 10:36:58 +0700

    pipe middleware filter validation

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,34 +1,56 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
+import { CreateSongDto } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongsController {
-    constructor(private songsService: SongsService) { }
-    
-    @Post()
-    create() {
-        return this.songsService.create("123");
-    }
+  constructor(private songsService: SongsService) {}
 
-    @Get()
-    findAll() {
-        return this.songsService.findAll(); 
-    }
+  @Post()
+  create(@Body() CreateSongDto: CreateSongDto) {
+    return this.songsService.create(CreateSongDto);
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `This action returns a #${id} song`;
+  @Get()
+  findAll() {
+    try {
+      return this.songsService.findAll();
+    } catch (error) {
+      throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: error,
+      });
     }
+  }
 
-    @Put(':id')
-    update(@Param('id') id: string) {
-        return `This action updates a #${id} song`;
-    }
+  @Get(':id')
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    return `This action returns a #${typeof id} song`;
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return `This action removes a #${id} song`;
-    }
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} song`;
+  }
 
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} song`;
+  }
 }
-
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -2,19 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SongsService {
-    private readonly songs = [
-        
-    ]
+  private readonly songs = [];
 
-    create(song) {
-        this.songs.push(song);
-        return this.songs;
-    }
+  create(song) {
+    this.songs.push(song);
+    return this.songs;
+  }
 
-    findAll() {
-        return this.songs;
-    }
-
+  findAll() {
+    // fetch the songs from the db
+    // errors comes while fetching the data
+    throw new Error('Error');
+    return this.songs;
+  }
 }
-
-
